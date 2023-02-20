178b5d970a981301b90728a8c18b556e1b0c37b3
flash0001 <ppo_it@mail.ru>
2023-02-20 21:46:06 +0300

    update files

--- device/timer.py
+++ device/timer.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+import asyncio
 from datetime import datetime
 
 
@@ -31,7 +32,7 @@ class Timer:
         return stop - start
 
     def stop(self):
-        if self._stop_at is None:
+        if self._start_at is not None and self._stop_at is None:
             self._stop_at = datetime.now().astimezone()
         return [self._start_at, self._stop_at]
 
@@ -40,17 +41,27 @@ class Timer:
 
 
 class CountdownTimer:
-    __FunctionType = type(lambda_: _)
+    __FunctionType = type(lambda _: _)
 
-    def __init__(self, seconds, callback=lambda *args, **kwargs: _):
+    def __init__(
+        self,
+        seconds,
+        callback=lambda *args, **kwargs: (args, kwargs),
+        *args,
+        **kwargs,
+    ):
         if isinstance(callback, CountdownTimer.__FunctionType):
             self.__callback = callback
+            self.__args = args
+            self.__kwargs = kwargs
+            self.__return = None
         else:
             t = type(callback)
             raise TypeError(f"callback must be function, not {t}")
         self.__is_running = False
         self.__timer = Timer()
         if isinstance(seconds, int) and seconds > 0:
+            self.__seconds = seconds
         elif isinstance(seconds, int) and seconds <= 0:
             raise ValueError(
                 "value of seconds argument must be greater than 0 but ti is {seconds}"
@@ -63,8 +74,16 @@ class CountdownTimer:
     def is_running(self):
         return self.__is_running
 
-    def start():
+    def start(self):
         if not self.__is_running:
             async def _():
                 self.__is_running = True
-            _()
+                while self.__timer.value.seconds < self.__seconds:
+                    pass
+                self.__return = self.__callback(*self.__args, **self.__kwargs)
+                self.__is_running = False
+            asyncio.run(_())
+
+    @property
+    def result(self):
+        return self.__return
