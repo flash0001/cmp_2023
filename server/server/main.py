4fe1496f3124da639863ad3e99c14db9b1274d68
flash0001 <ppo_it@mail.ru>
2023-03-16 22:24:37 +0300

    add the main file

--- server/server/main.py
+++ server/server/main.py
@@ -1,82 +1,99 @@
 #! /usr/bin/env python3
-from time import time
-from datetime import datetime
+import os
+import json
 import asyncio
-from websockets import serve
-from timer import Timer
+from flask import Flask, request, make_response, jsonify
+from core import ProcPool, Result
+from models import Race, ValidationError
+from subprocess import PIPE, Popen
 
 
-class BaseState:
-    def __init__(self, fsm):
-        self._fsm = fsm
+app = Flask("server")
+
+DEFAULT_CONFIG = {"port": 5555, "debug": True}
+
+pool = ProcPool()
+
+__dir__ = os.path.abspath(os.path.dirname(__file__))
+
+SUB_PROC_PATH = f"{__dir__}/core/device.py"
 
 
-class WaitingForRaceSate(fsm):
+class ClientError(Exception):
     pass
 
 
-class RaceState(fsm):
-    pass
+def read_config():
+    # TODO make the directory in home
+    home_dir = os.path.dirname(__file__)
+    cfg_file = os.path.join(home_dir, "config.json")
+    cfg = {**DEFAULT_CONFIG}
+    try:
+        with open(cfg_file) as fl:
+            data = json.load(fl)
+        cfg["port"] = data.get("port", DEFAULT_CONFIG["port"])
+        cfg["debug"] = data.get("debug", DEFAULT_CONFIG["debug"])
+    except (FileNotFoundError, json.JSONDecodeError):
+        with open(cfg_file, "w") as fp:
+            json.dump(DEFAULT_CONFIG, fp)
+    return cfg
 
 
-class FinishedRaceState(fsm):
-    pass
-
-
-class FiniteStateMachine:
-    def __init__(self):
-        self._wrs = WaitingForRaceSate(self)
-        self._rs = RaceState(self)
-        self._frs = FinishedRaceState(self)
-        self._current = self._wrs
-        self._timer = Timer()
-
-
-async def handler(websocket):
-    while True:
+@app.route("/race", methods=["POST"])
+def receive_race_params():
+    if pool.is_empty():
         try:
-            message = await websocket.recv()
-        except websockets.ConnectionClosedOK:
-            break
+            data = request.json
+            print(data)
+            race = Race(
+                race_type=data.get("race_type"),
+                drivers=data.get("drivers")
+            )
+            for driver_id in race.drivers:
+                pool.add(SUB_PROC_PATH, race.race_type, driver_id)
+            res = make_response(Result(ok="the race is running"))
+        except json.JSONDecodeError:
+            raise ClientError("content type error")
+        except ValidationError as err:
+            res = app.response_class(
+                response=json.dumps(Result(error=err)),
+                status=400,
+                mimetype='application/json'
+            )
+        except Exception as err:
+            res = app.response_class(
+                response=json.dumps(Result(error=err)),
+                status=500,
+                mimetype='application/json'
+            )
+    else:
+        res = app.response_class(
+            response=json.dumps(Result(error="the race is already running")),
+            status=404,
+            mimetype='application/json'
+        )
+    return res
 
-    async for message in websocket:
-        await websocket.send(message)
+
+@app.route("/race/status", methods=["GET"])
+def handler_state():
+    status = "finished" if pool.is_empty() else "running"
+    return jsonify(Result(ok=status))
 
 
-async def countdown(websocket):
-    start = datetime.now().astimezone()
-    async for i in range(60):
-        await websocket.send({"race": str(i+1), })
-        sleep(1)
-
-
-async def main():
-    async with serve(handler, "", 1024):
-        await asyncio.Future()
+@ app.route("/race/finish", methods=["GET"])
+def handler_unload():
+    if not pool.is_empty():
+        data = [*map(lambda res: res.stdout, pool.stop())]
+        res = jsonify(Result(ok=data))
+    else:
+        res = app.response_class(
+            response=json.dumps(Result(error="the race is already finished")),
+            status=404,
+            mimetype='application/json'
+        )
+    return res
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
-
-
-def validate_driver_id(idx: int):
-    if not isinstance(idx, int):
-        raise ClientError(f"driver_id must be int, but type is {type(idx)}")
-    elif idx < 1 or idx > 99:
-        raise ClientError("value of driver_id out of range")
-
-
-def validate_race_type(race_type):
-    if not isinstance(race_type, str):
-        raise ClientError(
-            f"race_type must be str, but type is {type(race_type)}")
-    if race_type not in RACE_TYPE:
-        raise ClientError("unknown type of race")
-
-
-def validate_user_data(data):
-    if not isinstance(data, list):
-        raise ClientError(f"data must be list, but received {type(data)}")
-    for item in data:
-        validate_driver_id(item.get("driver_id"))
-        validate_race_type(item.get("race_type"))
+    app.run(**read_config())
